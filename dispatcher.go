bd780d776eed0b0341cc2fa022541c613deb5e30
Max Kokorin <kokorin.max@gmail.com>
2015-01-23 20:02:02 +0600

    modify pattern

--- dispatcher.go
+++ dispatcher.go
@@ -1,32 +1,75 @@
 package main
 
-import "fmt"
+import (
+	//"errors"
+	"fmt"
+	"time"
+)
 
-var WorkerQueue chan chan WorkRequest
+var Dispatch *Dispatcher
 
-func StartDispatcher(nworkers int) {
-	// First, initialize the channel we are going to but the workers' work channels into.
-	WorkerQueue = make(chan chan WorkRequest, nworkers)
+type Dispatcher struct {
+	WorkerQueue chan chan WorkRequest
+	WorkQueue   chan WorkRequest
+	Workers     map[int]*Worker
+	QuitChan    chan bool
+}
+
+func NewDispatcher(nworkers int) error {
+
+	if Dispatch != nil {
+		//TODO: More errs handling here
+		//return errors.New("Dispatcher is already runned!")
+	}
+
+	Dispatch = &Dispatcher{
+		// First, initialize the channel we are going to but the workers' work channels into.
+		WorkerQueue: make(chan chan WorkRequest, nworkers),
+		WorkQueue:   make(chan WorkRequest, 100),
+		Workers:     make(map[int]*Worker),
+		QuitChan:    make(chan bool),
+	}
 
 	// Now, create all of our workers.
 	for i := 0; i < nworkers; i++ {
-		fmt.Println("Starting worker", i+1)
-		worker := NewWorker(i+1, WorkerQueue)
-		worker.Start()
+		fmt.Printf("Worker%d starting \r", i+1)
+		worker := NewWorker(i+1, Dispatch.WorkerQueue)
+		Dispatch.Workers[i+1] = worker
 	}
 
+	Dispatch.Start()
+	return nil
+}
+
+func (d *Dispatcher) Start() {
 	go func() {
 		for {
+
 			select {
-			case work := <-WorkQueue:
+			case work := <-d.WorkQueue:
 				fmt.Println("Received work requeust")
 				go func() {
-					worker := <-WorkerQueue
-
+					worker := <-d.WorkerQueue
 					fmt.Println("Dispatching work request")
 					worker <- work
 				}()
+			case <-d.QuitChan:
+				// We have been asked to stop.
+				for _, worker := range d.Workers {
+					worker.Stop()
+				}
+				fmt.Println("Dispatching stoped")
+				return
+			case <-time.After(5 * time.Second):
+				fmt.Printf("Dispatching of %v workers \n", len(d.Workers))
+
 			}
 		}
+	}()
+}
+
+func (d *Dispatcher) Stop() {
+	go func() {
+		d.QuitChan <- true
 	}()
 }
--- worker.go
+++ worker.go
@@ -8,14 +8,16 @@ import (
 // NewWorker creates, and returns a new Worker object. Its only argument
 // is a channel that the worker can add itself to whenever it is done its
 // work.
-func NewWorker(id int, workerQueue chan chan WorkRequest) Worker {
+func NewWorker(id int, workerQueue chan chan WorkRequest) *Worker {
 	// Create, and return the worker.
-	worker := Worker{
+	worker := &Worker{
 		ID:          id,
 		Work:        make(chan WorkRequest),
 		WorkerQueue: workerQueue,
-		QuitChan:    make(chan bool)}
+		QuitChan:    make(chan bool),
+	}
 
+	worker.Start()
 	return worker
 }
 
@@ -28,26 +30,24 @@ type Worker struct {
 
 // This function "starts" the worker by starting a goroutine, that is
 // an infinite "for-select" loop.
-func (w Worker) Start() {
+func (w *Worker) Start() {
 	go func() {
+
 		for {
 			// Add ourselves into the worker queue.
 			w.WorkerQueue <- w.Work
 
 			select {
 			case work := <-w.Work:
+
 				// Receive a work request.
-				fmt.Printf("worker%d: Received work request, delaying for %f seconds\n", w.ID, work.Delay.Seconds())
-
+				fmt.Printf("Worker%d: Received work request, delaying for %f seconds\n", w.ID, work.Delay.Seconds())
 				time.Sleep(work.Delay)
-				fmt.Printf("worker%d: Hello, %s!\n", w.ID, work.Name)
-
-			case <-time.After(1 * time.Second):
-				fmt.Printf("worker%d: run!\n", w.ID)
+				fmt.Printf("Worker: %d: Say Hello, %s!\n", w.ID, work.Name)
 
 			case <-w.QuitChan:
 				// We have been asked to stop.
-				fmt.Printf("worker%d stopping\n", w.ID)
+				fmt.Printf("Worker%d stopping\r", w.ID)
 				return
 			}
 		}
@@ -57,7 +57,7 @@ func (w Worker) Start() {
 // Stop tells the worker to stop listening for work requests.
 //
 // Note that the worker will only stop *after* it has finished its work.
-func (w Worker) Stop() {
+func (w *Worker) Stop() {
 	go func() {
 		w.QuitChan <- true
 	}()
